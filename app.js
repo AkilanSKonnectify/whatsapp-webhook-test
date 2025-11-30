1c6fe4d4931f6d8143ff92902996cc687e3cedba
Akilan <akilan@konnectify.co>
2025-11-30 10:02:37 +0000

    added alt url

--- app.js
+++ app.js
@@ -31,6 +31,26 @@ app.post('/', (req, res) => {
   res.status(200).end();
 });
 
+// Route for GET requests
+app.get('/alt', (req, res) => {
+  const { 'hub.mode': mode, 'hub.challenge': challenge, 'hub.verify_token': token } = req.query;
+
+  if (mode === 'subscribe' && token === verifyToken) {
+    console.log('WEBHOOK VERIFIED');
+    res.status(200).send(challenge);
+  } else {
+    res.status(403).end();
+  }
+});
+
+// Route for POST requests
+app.post('/alt', (req, res) => {
+  const timestamp = new Date().toISOString().replace('T', ' ').slice(0, 19);
+  console.log(`\n\nWebhook received ${timestamp}\n`);
+  console.log(JSON.stringify(req.body, null, 2));
+  res.status(200).end();
+});
+
 // Start the server
 app.listen(port, () => {
   console.log(`\nListening on port ${port}\n`);
